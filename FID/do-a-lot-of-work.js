8c7e345b896781600531cb750c05976abce91056
AlexanderShushunov <AShushunov@mail.ru>
2021-01-16 19:23:52 +0300

    Split the big task to a lot of small tasks.
    It get 100 points in LH and tiny (< 3 ms) FID.

--- FID/do-a-lot-of-work.js
+++ FID/do-a-lot-of-work.js
@@ -1,16 +1,46 @@
 let totalWidth = 0
 
+let counterId
+function generateId() {
+  return String(Math.ceil(Math.random()*10**10))
+}
+function counterNode() {
+  if (counterId === undefined) {
+    counterId = generateId()
+    const node = document.createElement('div')
+    node.setAttribute('id', counterId)
+    document.body.appendChild(node)
+  }
+  return document.getElementById(counterId)
+}
+
+let counter = 0
+function incCounter() {
+  counterNode().innerText = String(counter++)
+}
+
 function addNode () {
   const node = document.createElement('span')
   node.textContent = String(totalWidth)
   document.body.appendChild(node)
   totalWidth += node.getBoundingClientRect().width
+  incCounter()
 }
 
-function addALotOfNodes () {
+function clickButtonHandler (btn) {
+  btn.style.backgroundColor = "lightblue"
+}
+
+function addALotOfNodesSync () {
   for (let i = 0; i < 1000; i++) {
     addNode()
   }
 }
 
-addALotOfNodes()
+function addALotOfNodesAsync () {
+  for (let i = 0; i < 1000; i++) {
+    setTimeout(addNode, 0)
+  }
+}
+
+
